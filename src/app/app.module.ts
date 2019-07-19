fcaab26681db23c4511816702812b20bbd7c8d58
Battistini <battistini.anthony@gmail.com>
2019-07-19 03:06:20 +0200

    Config AppModule for Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,34 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppComponent} from './app.component';
-import {environment} from '../environments/environment';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {APP_CONFIG} from './app.config';
-import {TokenInterceptorService} from './services/interceptors/token-interceptor.service';
-import {ErrorInterceptorService} from './services/interceptors/error-interceptor.service';
-import {CommonHeadersInterceptorService} from './services/interceptors/common-headers-interceptor.service';
+import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { APP_CONFIG } from './app.config';
+import { TokenInterceptorService } from './services/interceptors/token-interceptor.service';
+import { ErrorInterceptorService } from './services/interceptors/error-interceptor.service';
+import { CommonHeadersInterceptorService } from './services/interceptors/common-headers-interceptor.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WelcomeComponent } from './components/welcome/welcome.component';
+import { RouterModule } from '@angular/router';
+import { ROUTES } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(ROUTES),
   ],
   providers: [
-    {provide: APP_CONFIG, useValue: environment},
-    {provide : HTTP_INTERCEPTORS, useClass : CommonHeadersInterceptorService, multi: true},
-    {provide : HTTP_INTERCEPTORS, useClass : TokenInterceptorService, multi: true},
-    {provide : HTTP_INTERCEPTORS, useClass : ErrorInterceptorService, multi: true},
+    { provide: APP_CONFIG, useValue: environment },
+    { provide: HTTP_INTERCEPTORS, useClass: CommonHeadersInterceptorService, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptorService, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptorService, multi: true },
   ],
   bootstrap: [AppComponent]
 })
